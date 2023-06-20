db51d2fd3a267c2a222d01c7734e12a0c6be4af7
InternetOG <omoleyeosadare@gmail.com>
2023-06-20 16:09:31 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs -  absolute value of a number
+ *
+ * @num: integer number
+ *
+ * Return: return -num or num
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
