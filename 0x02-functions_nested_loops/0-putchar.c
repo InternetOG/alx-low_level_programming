f42e6adb2f2d2c978f62b7224dda1110bd986fe6
InternetOG <omoleyeosadare@gmail.com>
2023-06-20 11:55:56 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char putChar[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(putChar[i]);
+	}
+
+	return (0);
+}
