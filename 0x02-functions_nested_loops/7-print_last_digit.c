aa1eb2394dd7fa2af397f1433a80bd0a0dd2623e
InternetOG <omoleyeosadare@gmail.com>
2023-06-20 16:57:30 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * printf_last_digit - prints the last number
+ * print_last_digit - prints the last number
  *
  * @num: integer variable
  *
@@ -10,6 +10,6 @@
 int print_last_digit(int num)
 {
 	int last_digit = num % 10;
-	return (last_digit);
 
+	return (last_digit);
 }
