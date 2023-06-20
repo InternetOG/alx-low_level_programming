16dd5b513176432b4ec50db48f3e9c404e52b3ae
InternetOG <omoleyeosadare@gmail.com>
2023-06-20 16:52:46 -0700

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * printf_last_digit - prints the last number
+ *
+ * @num: integer variable
+ *
+ * Return: last_digit
+ */
+
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+	return (last_digit);
+
+}
