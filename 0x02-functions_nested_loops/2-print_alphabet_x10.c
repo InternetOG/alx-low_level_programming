95884da9cdb2937d8dc3d1be9c876b99812f4210
InternetOG <omoleyeosadare@gmail.com>
2023-06-20 13:21:53 -0700

    10xAlphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a-z 10 times
+ *
+ * Return: return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
