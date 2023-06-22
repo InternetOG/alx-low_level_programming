fb53b8738514d04a2a29db0f4c4958a95f0e1e0a
InternetOG <omoleyeosadare@gmail.com>
2023-06-22 12:48:58 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 11; i++)
 	{
-		for (int j = 0; j <= 14; i++)
+		for (int j = 0; j < 15; j++)
 		{
-			if (j > 9)
+			if (j > 9 && j < 15)
 			{
 				_putchar((j / 10) + '0');
 			}
