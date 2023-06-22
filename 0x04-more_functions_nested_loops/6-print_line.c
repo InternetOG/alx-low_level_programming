42be75d7895390293fceaa9c2fb67978668028a7
InternetOG <omoleyeosadare@gmail.com>
2023-06-22 13:12:26 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,3 +1,14 @@
+#include  "main.h"
+
+/**
+ * print_line - prints underscore
+ *
+ * @n: integer type
+ *
+ * Return: always 0
+ *
+ */
+
 void print_line(int n){
         for(int i = 0; i < n; i++){
                 _putchar('_');
