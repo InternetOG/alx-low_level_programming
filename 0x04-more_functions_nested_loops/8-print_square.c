afc985e7a2fb2286e94b67b6f144d150ece25794
InternetOG <omoleyeosadare@gmail.com>
2023-06-22 15:42:28 -0700

    fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,10 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size == 0 || size < 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -20,4 +24,5 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
+	putchar('\n');
 }
