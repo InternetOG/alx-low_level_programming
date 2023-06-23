e3e0e599c32236deb284bbf51af12ee640093b37
InternetOG <omoleyeosadare@gmail.com>
2023-06-23 12:13:08 -0700

    test

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
- * print_line - display backward slash
+ *print_diagonal - draws diagonal
  *
- * @n: type int
- *
- * Return: always 0
+ *@n: int type parameter
+ *Return: prints diagonal line
  */
 
-void print_line(int n){
-	int i;
+void print_diagonal(int n)
+{
+	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-                _putchar('\\');
-        }
-        _putchar('\n');
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
