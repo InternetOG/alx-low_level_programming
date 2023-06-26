572e72dc9b4a8ef88b4258ceab0e760a412ea52a
InternetOG <omoleyeosadare@gmail.com>
2023-06-26 12:51:03 -0700

    6-puts

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - display string of char in reverse
+ *
+ * @s: This is pointer variable
+ *
+ * Return: always 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	printf("Value of I: %i", i);
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		s[i];
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		 s[i];
+	}
+	s;
+}
