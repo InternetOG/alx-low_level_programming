eae129622a33e8076159c48d8b4b9d228725d76a
InternetOG <omoleyeosadare@gmail.com>
2023-06-27 05:03:13 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcpy - this copies a string to another
+ *
+ * @src: The source variable
+ * @dest: the destination variable
+ *
+ * Return: always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
