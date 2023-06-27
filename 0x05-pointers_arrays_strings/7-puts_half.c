849c77826abd65730165a3f082115b8ce1123449
InternetOG <omoleyeosadare@gmail.com>
2023-06-27 03:46:09 -0700

    7_puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - divides the length of char and print other half
+ *
+ * @str: pointer variable
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
+	}
+	for (j = (i / 2); j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
