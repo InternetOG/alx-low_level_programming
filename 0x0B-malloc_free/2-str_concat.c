21b01ea9f7cb3b01db43c752d3e66b1586faa7d2
InternetOG <omoleyeosadare@gmail.com>
2023-07-11 05:27:28 -0700

    happy

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,32 +19,25 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	j = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-
 	while (s1[i] != '\0')
 	{
 		i++;
 	}
-
 	while (s2[j] != '\0')
 	{
 		j++;
 	}
-
 	len = i + j;
-
 	s = malloc(len * sizeof(char));
-
 	for (k = 0; k < i; k++)
 	{
 		s[k] = s1[k];
 	}
-
 	m = 0;
-
 	for (l = k; l < len; l++)
 	{
 		s[l] = s2[m];
