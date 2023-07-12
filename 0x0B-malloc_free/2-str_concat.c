e50135b0c7170281b29c158468984b95de9836b7
InternetOG <omoleyeosadare@gmail.com>
2023-07-12 08:40:56 -0700

    2 -str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,11 +31,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		j++;
 	}
-	printf("%i\n", i);
-	printf("%i\n", j);
 	len = i + j;
-	printf("%i\n", len);
-	s = malloc((len + 1) * sizeof(char));
+	s = (char *)malloc((len + 1) * sizeof(char));
 	if (s == NULL)
 	{
 		return (NULL);
