2e949e540ca4d40c52b0812aa9dfcc13151669a3
InternetOG <omoleyeosadare@gmail.com>
2023-07-12 21:58:11 -0700

    3-alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,16 +2,42 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * create_array - create array using malloc
- * @size: total length of elements
- * @c: character assigned to each element
+ * alloc_grid - create array using malloc
+ * @width: total length of elements
+ * @height: character assigned to each element
  * Return: pointer s
  */
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int j;
-	int *s;
+	int i, j, **s;
 
-	s = mallocdd()
+	if ((width <= 0 || height <= 0))
+	{
+		return (NULL);
+	}
+	s = (int **)malloc((width * height) * sizeof(int));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		s[i] = (int *)malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(s[i]);
+			}
+			free(s);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+	return (s);
+	free(s);
 }
