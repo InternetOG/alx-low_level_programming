516ab0e36c593dcc94a780ff1d67a617b409b0e5
InternetOG <omoleyeosadare@gmail.com>
2023-07-27 21:37:58 -0700

    0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * print_list - linked list function to print elements and size
+ * @h: a pointer to a struct typedef to list_t
+ * Return: count
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h != NULL)
+	{
+		const list_t *new = NULL;
+
+		new = h;
+
+		if (new != NULL)
+		{
+			while (new)
+			{
+				if (new->str != NULL)
+				{
+					printf("[%i] %s\n", new->len, new->str);
+				}
+				else
+				{
+					printf("[0] (nil)\n");
+				}
+				new = new->next;
+				count++;
+			}
+		}
+	}
+	return (count);
+}
+
