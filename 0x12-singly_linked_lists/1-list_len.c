90743631df9bf1c4a98a7fa1385cef535059738d
InternetOG <omoleyeosadare@gmail.com>
2023-07-27 21:54:52 -0700

    1-list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - linked list function to print elements and size
+ * @h: a pointer to a struct typedef to list_t
+ * Return: count
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h != NULL)
+	{
+		const list_t *new = NULL;
+
+		new = h;
+
+		if (new != NULL)
+		{
+			while (new)
+			{
+				new = new->next;
+				count++;
+			}
+		}
+	}
+	return (count);
+}
+
