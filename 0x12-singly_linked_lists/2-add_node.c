0037b04457ff1451be9cb1effabde973c7ef95e7
InternetOG <omoleyeosadare@gmail.com>
2023-07-28 17:13:59 -0700

    2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,8 +4,8 @@
 #include <string.h>
 /**
  * add_node -  a pointer to struct node
- * head: double pointer to a struct
- * str: a pointer to a string
+ * @head: double pointer to a struct
+ * @str: a pointer to a string
  * Return: return a pointer to a struct
  */
 list_t *add_node(list_t **head, const char *str)
@@ -21,7 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 		(*head) = firstNode;
 		return (firstNode);
 	}
-	else 
+	else
 	{
 		return (NULL);
 	}
