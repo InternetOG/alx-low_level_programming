f8acc189382a3268bd45ba3983eaa241ed19991e
InternetOG <omoleyeosadare@gmail.com>
2023-07-31 12:31:12 -0700

    2-add_nodeint

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,9 +13,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp;
 
+	temp = NULL;
 	temp = malloc(sizeof(listint_t));
-	temp->n = n;
-	temp->next = (*head);
-	(*head) = temp;
+	if (temp != NULL)
+	{
+		temp->n = n;
+		temp->next = (*head);
+		(*head) = temp;
+	}
 	return ((*head));
 }
