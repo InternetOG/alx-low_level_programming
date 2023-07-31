ef3afc837a1ee51305821b443ef137201d24d680
InternetOG <omoleyeosadare@gmail.com>
2023-07-31 11:58:22 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - linked list function to print elements and size
+ * @head: a pointer to a struct typedef to list_t
+ * @n: a const int
+ * Return: count
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+	temp->n = n;
+	temp->next = (*head);
+	(*head) = temp;
+	return ((*head));
+}
