98d0f6fadb4c9341e3011a0b7452599bba8ec33e
InternetOG <omoleyeosadare@gmail.com>
2023-08-01 14:40:12 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
- * print_listint - linked list function to print elements and size
- * @h: a pointer to a struct typedef to list_t
- * Return: count
- */
-
+* print_listint - function
+* @h:  argument
+* Return: returns sum
+*
+*/
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+int sum = 0;
 
-	if (h != NULL)
-	{
-		const listint_t *temp = NULL;
-
-		temp = malloc(sizeof(listint_t));
-		temp = h;
-
-		while (temp != NULL)
-		{
-			printf("%i\n", temp->n);
-			temp = temp->next;
-			count++;
-		}
-	}
-	return (count);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+sum += 1;
+h = h->next;
+}
+return (sum);
 }
