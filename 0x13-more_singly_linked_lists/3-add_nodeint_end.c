ef534de3c84478555a05e37adbeeb30e35a56789
InternetOG <omoleyeosadare@gmail.com>
2023-07-31 20:08:40 -0700

    3-add_nodeint_end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - linked list function to print elements and size
+ * @head: a pointer to a struct typedef to list_t
+ * @n: a const int
+ * Return: count
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp;
+	listint_t *newNode = NULL;
+
+	newNode = (listint_t *)malloc(sizeof(listint_t));
+	if (newNode != NULL)
+	{
+		newNode->n = n;
+		newNode->next = NULL;
+	}
+
+	if ((*head) == NULL)
+	{
+		(*head) = temp = newNode;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newNode;
+		temp = newNode;
+	}
+	return ((*head));
+}
