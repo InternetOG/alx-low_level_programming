c3a33a5ea4f0afbf13446e5e76649df7bb1b1db1
InternetOG <omoleyeosadare@gmail.com>
2023-08-06 09:12:20 -0700

    1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,63 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
 /**
-* binary_to_uint - function to convert binary to decimal
-* @b: argument in the function
+* print_binary - function to convert binary to decimal
+* @n: argument in the function
 * Return: returns value
 */
 
 void print_binary(unsigned long int n)
 {
-	char *binary, *revBin;
-	int wNum;
-    	int rmd;
 	int i;
-	int ph;
 
-    	int index = 0;
-	binary = NULL;
-	revBin = NULL;
-	binary = malloc(sizeof(char));
 
-	if (binary == NULL)
-	{
-		return;
-	}
+		for (i = 10; i >= 0; i--)
+		{
+			if (n & (1 << i))
+					printf("1");
+			else
+				printf("0");
+		}
 
-	if (n == 0)
-	{
-		printf("0");
-	}
-    while (n)
-    {
-        wNum = n / 2;
-        rmd = n % 2;
-        binary[index] = rmd;
-        n = wNum;
-        index++;
-    }
-    binary[index] = '\0';
-
-    revBin = malloc(sizeof(char));
-
-    if (revBin == NULL)
-    {
-        return;
-    }
-
-    ph = index - 1;
-    for (i = 0; i < index; i++)
-    {
-        revBin[i] = binary[ph];
-        ph--;
-    }
-
-     for (i = 0; i < index; i++)
-    {
-        printf("%i", revBin[i]);
-    }
-     free(binary);
-     free(revBin);
 }
+
