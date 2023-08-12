528b4e1a6a23303b8d25d67c4c4541c31b764e15
InternetOG <omoleyeosadare@gmail.com>
2023-08-12 12:23:02 -0700

    InternetOG

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,28 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    int res= 0;
-    int i = 0;
-    int j = 0;
-    int count = 0;
+	int res = 0;
+	int i = 0;
+	int j = 0;
+	int count = 0;
 
-    if (b != NULL)
-    {
-        while (b[i] != '\0')
-        {
-            count++;
-            i++;
-        }
+	if (b != NULL)
+	{
+		while (b[i] != '\0')
+		{
+			count++;
+			i++;
+		}
 
-        while (b[j] != '\0')
-        {
-            if (b[j] > 49)
-            {
-                return (0);
-            }
-            res += (b[j] - '0') << ((i - j) - 1);
-            j++;
-        }
-    }
-    return (res);
+		while (b[j] != '\0')
+		{
+			if (b[j] > 49)
+			{
+				return (0);
+			}
+			res += (b[j] - '0') << ((i - j) - 1);
+			j++;
+		}
+	}
+	return (res);
 }
