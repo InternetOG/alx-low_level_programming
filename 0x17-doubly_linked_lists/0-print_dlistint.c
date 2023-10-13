93699bc77e9c54cc1e13670842f7072de2c4ed29
InternetOG <omoleyeosadare@gmail.com>
2023-10-13 16:12:01 +0100

    edit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,25 +1,23 @@
-  #include "lists.h"
+#include "lists.h"
 
 /**
- * print_dlistint - prints the length of a doubly linked list
- * @h: The head node
- *
- * Return: returns the number in lenght
+ * print_dlistint - print the data in a double linked list
+ * @h: head node or start node
+ * Return: returns the number of node
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n = 0;
+	size_t n = 0;
 
-    if (h != NULL)
-    {
-        while (h != NULL)
-        {
-            n += 1;
-            printf("%i\n", h->n);
-            h = h->next;
-        }
-        return (n);
-    }
-    return (n);
+	if (h != NULL)
+	{
+		while (h != NULL)
+		{
+			n += 1;
+			printf("%i\n", h->n);
+			h = h->next;
+		}
+		return (n);
+	}
+	return (n);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,18 +5,18 @@
  *
  * Return: returns the number in lenght
  */
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
-size_t n = 0;
+	size_t n = 0;
 
-if (h != NULL)
-{
-while (h != NULL)
-{
-n += 1;
-h = h->next;
-}
-return (n);
-}
-return (n);
+	if (h != NULL)
+	{
+		while (h != NULL)
+		{
+			n += 1;
+			h = h->next;
+		}
+		return (n);
+	}
+	return (n);
 }
